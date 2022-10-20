6f75bc2b3d6b2b049e67177d5aee77e0859dacb8
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2022-10-20 11:11:55 -0300

    Adding exercise 1, have error in 1000.in.txt

--- Ejercicio1/ejercicio1.cpp
+++ Ejercicio1/ejercicio1.cpp
@@ -3,8 +3,228 @@
 #include <iostream>
 using namespace std;
 
+struct NodoLista
+{
+    string clave;
+    int valor;
+    NodoLista *sig;
+    NodoLista(string unaClave, int unValor) : clave(unaClave), valor(unValor), sig(0){};
+    NodoLista(string unaClave, int unValor, NodoLista *unSig) : clave(unaClave), valor(unValor), sig(unSig){};
+};
+
+class TablaHashCerrada
+
+{
+private:
+    typedef NodoLista *List;
+    List *tabla;
+    int tam;
+
+    float factorCarga()
+    {
+        return (float)this->numElem / this->tam;
+    };
+
+    bool insert(List *tabla, string clave, int valor, int pos)
+    {
+        if (tabla[pos] == NULL)
+        {
+            numElem++;
+            tabla[pos] = new NodoLista(clave, valor);
+            return true;
+        }
+        else
+        {
+            if (tabla[pos]->clave == clave)
+            {
+                tabla[pos]->valor += valor;
+                return false;
+            }
+            else
+            {
+                pos++;
+                if (pos == this->tam)
+                    pos = 0;                
+                return insert(tabla, clave, valor, pos);
+            }
+        }
+    };
+
+    bool deleteElem(string clave, List &list)
+    {
+        if (list == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            if (list->clave == clave)
+            {
+                List aux = list;
+                list = list->sig;
+                delete aux;
+                return true;
+            }
+            else
+            {
+                return deleteElem(clave, list->sig);
+            }
+        }
+    };
+
+    bool keyExists(string clave, NodoLista *list)
+    {
+        if (list == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            if (list->clave == clave)
+            {
+                return true;
+            }
+            else
+            {
+                return keyExists(clave, list->sig);
+            }
+        }
+    };
+
+    bool getKey(string clave, List list)
+    {
+        if (list == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            if (list->clave == clave && list->valor > 0)
+            {
+                list->valor--;
+                return true;
+            }
+            else
+            {
+                return getKey(clave, list->sig);
+            }
+        }
+    };
+
+    int funcionHash(string clave, int miTam)
+    {
+        int suma = 0;
+        for (int i = 0; i < clave.length(); i++)
+        {
+            suma += int(clave[i]);
+        }
+        return (suma % miTam);
+    };
+
+public:
+    int numElem;
+    TablaHashCerrada(int unTam)
+    {
+        this->tam = unTam;
+        this->tabla = new List[this->tam];
+        for (int i = 0; i < this->tam; i++)
+        {
+           this->tabla[i] = NULL;
+        }
+        numElem = 0;
+    };
+
+    void rehash()
+    {
+        List *nuevaTabla = new List[tam * 2];
+        for (int i = 0; i < tam * 2; i++)
+        {
+            nuevaTabla[i] = NULL;
+        }
+        numElem = 0;
+
+        for (int i = 0; i < tam; i++)
+        {
+            List list = tabla[i];
+            if(list != NULL)
+            {
+                    insert(nuevaTabla, list->clave, list->valor, funcionHash(list->clave, (tam * 2)));
+            }
+        }
+        tabla = nuevaTabla;
+        tam = tam * 2;
+    };    
+
+    void insertar(string clave, int valor)
+    {
+        int pos = abs(funcionHash(clave, tam)) % tam;
+        if (insert(tabla, clave, valor, pos))
+        {
+            if (factorCarga() > 0.5)
+            {
+                rehash();
+            }
+        }
+    };
+
+    void deleteElem(string clave)
+    {
+        int pos = abs(funcionHash(clave, tam)) % tam;
+        if (deleteElem(clave, tabla[pos]))
+        {
+            numElem--;
+        }
+    };
+
+    bool existe(string clave)
+    {
+        int pos = abs(funcionHash(clave, tam)) % tam;
+        clave = toupper(clave[0]);
+        
+        return keyExists(clave, tabla[pos]);
+    };
+
+    bool getKey(string clave)
+    {
+        int pos = abs(funcionHash(clave, tam)) % tam;
+        return getKey(clave, tabla[pos]);
+    };
+
+    void imprimir()
+    {
+        for (int i = 0; i < tam; i++)
+        {
+            cout << i << ": ";
+            NodoLista *list = tabla[i];
+            while (list != NULL)
+            {
+                cout << list->clave << " ";
+                cout << list->valor << " ";
+                list = list->sig;
+            }
+            cout << endl;
+        }
+    };
+};
+
+
 int main()
 {
-    cout << "Works!!! gg ease" << endl;
+    int users;
+    string email;
+
+    cin >> users;
+
+    TablaHashCerrada tabla(users);
+
+    while (users > 0)
+    {
+        cin >> email;
+        tabla.insertar(email, 1);
+        users--;
+    }
+    
+   cout << tabla.numElem << endl;
+
     return 0;
 }
